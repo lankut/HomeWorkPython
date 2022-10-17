fb277be55d99f8efe30cd1e0e1ec0660fe74409f
lankute <lankute@mail.ru>
2022-10-17 08:49:39 +0300

    HomeWork 5 added

--- task3.py
+++ task3.py
@@ -1,10 +1,22 @@
-# Задайте последовательность цифр. Напишите программу, которая выведет список неповторяющихся элементов
-# исходной последовательности.
+# Создайте программу для игры в 'Крестики-нолики'.
 
-list = list(map(int, input("Введите числа через пробел: ").split()))
-print(list)
-new_list = []
-for i in list:
-    if i not in new_list:
-        new_list.append(i)
-print(f'Список из неповторяющихся элементов: {new_list}')
+position=[]
+for i in range(1,10):
+    position.append(i)
+
+sign = '0'
+
+while True:
+    if sign == '0':
+        sign = 'x'
+    else:
+        sign = '0'
+    print(f'{position[0]:^5}|{position[1]:^5}|{position[2]:^5}')
+    print('-----------------')
+    print(f'{position[3]:^5}|{position[4]:^5}|{position[5]:^5}')
+    print('-----------------')
+    print(f'{position[6]:^5}|{position[7]:^5}|{position[8]:^5}')
+
+    index = int(input(f"\n\nХод {'игрока' if sign == 'x' else 'противника'}: "))
+
+    position[index-1] = sign
--- task4.py
+++ task4.py
@@ -1,33 +1,31 @@
-# Задача №33: Задана натуральная степень k.
-# Сформировать случайным образом список коэффициентов (значения от -100 до 100) многочлена и записать в файл многочлен степени k.
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. 
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
 
-import random
+def coding(txt):
+    count = 1
+    res = ''
+    for i in range(len(txt)-1):
+        if txt[i] == txt[i+1]:
+            count += 1
+        else:
+            res = res + str(count) + txt[i]
+            count = 1
+    if count > 1 or (txt[len(txt)-2] != txt[-1]):
+        res = res + str(count) + txt[-1]
+    return res
 
-k = int(input('Введите значение натуральной степени: '))
-print(k)
+def decoding(txt):
+    number = ''
+    res = ''
+    for i in range(len(txt)):
+        if not txt[i].isalpha():
+            number += txt[i]
+        else:
+            res = res + txt[i] * int(number)
+            number = ''
+    return res
 
-list = []
 
-for i in range(k):
-    list.append(random.randint(-100, 100))
-print(list)
-
-list_2 = f'k = {k} -> '
-
-for i in range(len(list)):
-    if i != len(list) - 1 and list[i] != 0 and i != len(list) - 2:
-        list_2 += f'{list[i]}x^{len(list)-i-1}'
-        if list[i+1] != 0 and list[i+1] > 0:
-            list_2 += '+'
-    elif i == len(list) - 2 and list[i] != 0:
-        list_2 += f'{list[i]}x'
-        if list[i+1] != 0 and list[i+1] > 0:
-            list_2 += '+'
-    elif i == len(list) - 1 and list[i] != 0:
-        list_2 += f'{list[i]} = 0'
-    elif i == len(list) - 1 and list[i] == 0:
-        list_2 += ' = 0'
-print(list_2)
-
-with open ('res.txt', 'w') as file:
-    file.write(list_2)
+s = input("Введите текст для кодировки: ")
+print(f"Текст после кодировки: {coding(s)}")
+print(f"Текст после дешифровки: {decoding(coding(s))}")
